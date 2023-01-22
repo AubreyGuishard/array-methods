3f4450e1fe5e3185cbd4515ffc706de9c109ee27
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-01-22 13:38:59 -0500

    Completed Problem One Creating a Function that will return the Cuisine Type of Vegetarian

--- app.js
+++ app.js
@@ -113,13 +113,16 @@ let dishes = [
 ]
 
 //Example function
-//IMPORTANT: Take the time to step through this example function with a breakpoint until you could explain what is going on to someone else before starting this lab.
+//IMPORTANT: Take the time to step through this example function with a breakpoint until you could explain what is going on to 
+//someone else before starting this lab.
 
 function filterExample(){
-    //Debug tip: Use a console.log(el) inside the filter function to get a visualization of what el represents and see all its properties! This helps you to know what you can access with dot notation inside the filter. Do this every time you use a .filter or else you are working in the dark!
+    //Debug tip: Use a console.log(el) inside the filter function to get a visualization of what el represents and see all 
+    //its properties! This helps you to know what you can access with dot notation inside the filter. 
+    //Do this every time you use a .filter or else you are working in the dark!
     let results;
     results = dishes.filter(function(el){
-        console.log("el inside filterExample's filter: ", el)
+        console.log("dish inside filterExample's filter: ", el)
         if(el.cuisine === "Mexican"){
             return true;
         }
@@ -131,8 +134,8 @@ function filterExample(){
 }
 
 let mexicanFood = filterExample();
-console.log('mexicanFood from filterExample', mexicanFood)
-
+//console.log('mexicanFood from filterExample', mexicanFood)
+//console.log(dish)
 
 
 //Reminder: Do not move on to problem one until understand the example completely!!
@@ -141,19 +144,31 @@ console.log('mexicanFood from filterExample', mexicanFood)
 
 function problemOne(){
 
-    let results;
+    let results = dishes.filter(function(dish){
+        console.log("dish inside filterExample's filter: ", dish)
+        if(dish.cuisine === "Vegetarian"){
+            return true;
+        }
+    })
 
     return results;
 }
 
+let vegetarianFood = problemOne();
+console.log('vegetarianFood from filter', vegetarianFood)
+
 //2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
 //Filter
+function problemTwo(){
 
+} 
 
 
 //3. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
-
+function problemThree(){
+    
+}
 
 
 //4. Create a function that will return only dishes whose id number matches their serving count.
